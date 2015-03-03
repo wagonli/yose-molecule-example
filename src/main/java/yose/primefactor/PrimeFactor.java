7da3f460f941a7d2b37af7590cbdabd711c9b0a6
formation agile <sachetpa@gmail.com>
2015-03-03 16:19:39 +0100

    code cleaning / refactoring

--- src/main/java/yose/primefactor/PrimeFactor.java
+++ src/main/java/yose/primefactor/PrimeFactor.java
@@ -12,17 +12,10 @@ public class PrimeFactor {
     public static Integer[] powerOfTwoDecomposition(int power) {
      
             List<Integer> result = new
-                    ArrayList<Integer>();
+                    ArrayList<>();
             for (int remainder=power; remainder>=2; remainder=remainder/2) {
                 result.add(2);
             }
             return result.toArray(new Integer[result.size()]);
-
-
-
-
-
-
-
     }
 }

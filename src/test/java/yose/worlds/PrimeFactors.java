4d6e0e95a5cc91f00be2e89528ca21f3c2181fc4
formation agile <sachetpa@gmail.com>
2015-03-03 16:24:00 +0100

    code cleaning -> created PrimeFactors world test

---
+++ src/test/java/yose/worlds/PrimeFactors.java
@@ -0,0 +1,41 @@
+package yose.worlds;
+
+import com.vtence.molecule.testing.http.HttpRequest;
+import com.vtence.molecule.testing.http.HttpResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import yose.YoseDriver;
+import yose.pages.GitHubPage;
+
+import java.io.IOException;
+
+import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
+
+public class PrimeFactors {
+
+    YoseDriver yose = new YoseDriver(9999);
+
+    HttpRequest request = new HttpRequest(9999);
+    HttpResponse response;
+
+    @Before
+    public void startGame() throws Exception {
+        yose.start();
+    }
+
+    @After
+    public void stopGame() throws Exception {
+        yose.stop();
+    }
+
+    @Test
+    public void powerOfTwoChallenge() throws IOException {
+        response = request.get("/primeFactors?number=4");
+
+        assertThat(response).isOK()
+                .hasContentType("application/json")
+                .hasBodyText("{\"number\":4,\"decomposition\":[2,2]}");
+    }
+
+}

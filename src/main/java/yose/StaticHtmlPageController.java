2dc7f2e35288a2e0c713160c67aea0554402a7a1
Fred M <frederic.mazeiras@orange.com>
2015-03-04 16:09:27 +0100

    Portfolio - Ping refoctor HomeControler / StaticHtmlPageController

---
+++ src/main/java/yose/StaticHtmlPageController.java
@@ -0,0 +1,25 @@
+package yose;
+
+import com.vtence.molecule.Request;
+import com.vtence.molecule.Response;
+import com.vtence.molecule.lib.FileBody;
+
+import java.io.File;
+
+/**
+ * Created by formation on 04/03/15.
+ */
+public class StaticHtmlPageController {
+    private File pageFile;
+
+    public StaticHtmlPageController(File file) {
+
+        pageFile = file;
+    }
+
+    public void setupHtmlPage(Request request, Response response) {
+
+        response.contentType("text/html");
+        response.body(new FileBody(pageFile));
+    }
+}
--- src/test/java/yose/controler/StaticHtmlPageControllerTest.java
+++ src/test/java/yose/controler/StaticHtmlPageControllerTest.java
@@ -4,29 +4,31 @@ import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
 import org.hamcrest.Matchers;
 import org.junit.Test;
-import yose.HomeController;
+import yose.StaticHtmlPageController;
+
+import java.io.File;
 
 import static com.vtence.molecule.testing.ResponseAssert.assertThat;
 
 /**
  * Created by formation on 04/03/15.
  */
-public class HomeControllerTest {
+public class StaticHtmlPageControllerTest {
 
 
     private Response response = new Response();
-    private HomeController homeController = new HomeController();
+    private StaticHtmlPageController staticHtmlPageController = new StaticHtmlPageController(new File("src/main/webapp/HomePage.html"));
     private Request request= new Request();
 
     @Test
     public void alwaysReturnHTMLContent() {
-        homeController.getHomePage(request, response);
+        staticHtmlPageController.setupHtmlPage(request, response);
         assertThat(response).hasContentType("text/html");
     }
 
     @Test
     public void hasNotEmptyBodyResponse() {
-        homeController.getHomePage(request, response);
+        staticHtmlPageController.setupHtmlPage(request, response);
         assertThat(response).hasBodyText(Matchers.not(Matchers.isEmptyOrNullString()));
     }
 

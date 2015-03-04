5307a9957b9b36748d1a42f5c02d0154f063490e
formation agile <sachetpa@gmail.com>
2015-03-04 11:19:59 +0100

    Updated gradle build to use latest selenium & DOMMatchers

---
+++ src/test/java/yose/worlds/Astroport.java
@@ -0,0 +1,61 @@
+package yose.worlds;
+
+import com.vtence.molecule.testing.http.HttpRequest;
+import com.vtence.molecule.testing.http.HttpResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import yose.YoseDriver;
+import yose.pages.GitHubPage;
+
+import java.io.IOException;
+
+import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
+
+public class Astroport {
+
+    YoseDriver yose = new YoseDriver(9999);
+
+    HttpRequest request = new HttpRequest(9999);
+    HttpResponse response;
+
+    @Before
+    public void startGame() throws Exception {
+        yose.start();
+    }
+
+    @After
+    public void stopGame() throws Exception {
+        yose.stop();
+    }
+
+    @Test
+    public void firstWebPageChallenge() throws IOException {
+        yose.home().displaysMessage("Hello Yose");
+    }
+
+    @Test
+    public void firstWebServiceChallenge() throws IOException {
+        response = request.get("/ping");
+
+        assertThat(response).isOK()
+                            .hasContentType("application/json")
+                            .hasBodyText("{\"alive\":true}");
+    }
+
+    @Test
+    public void shareWebPageChallenge() throws IOException {
+        GitHubPage gitHubPage = yose.home().openGithub();
+        gitHubPage.seesInReadme("YoseTheGame");
+    }
+
+    @Test
+    public void powerOfTwoChallenge() throws IOException {
+        response = request.get("/primeFactors?number=4");
+
+        assertThat(response).isOK()
+                .hasContentType("application/json")
+                .hasBodyText("{\"number\":4,\"decomposition\":[2,2]}");
+    }
+
+}
